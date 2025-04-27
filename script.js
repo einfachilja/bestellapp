296d4882199a009fbff7b998d6380ee2972f4b0e
Ilja Völker-Savkiv <ilja@MacBookAir.fritz.box>
2025-04-27 21:29:32 +0200

    added addToShoppingBasket() & renderShoppingBasket()

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let shoppingBasket = [];
+
 function render() {
     let contentRef = document.getElementById('content');
     contentRef.innerHTML = '';
@@ -6,12 +8,29 @@ function render() {
         const NAME = dishes[index].name;
         const INFO = dishes[index].info;
         const PRICE = dishes[index].price;
-
-        contentRef.innerHTML += getDishesTemplate(NAME, INFO, PRICE);
-
+        contentRef.innerHTML += getDishesTemplate(NAME, INFO, PRICE, index);
     }
 }
 
 function toggleShopingBasket() {
     document.getElementById('page_style_side').classList.toggle('d-none');
 }
+
+function addToShoppingBasket(index) {
+    shoppingBasket.push(dishes[index]);
+    renderShoppingBasket();
+}
+
+function renderShoppingBasket() {
+    let shoppingBasketRef = document.getElementById('basket_content');
+    shoppingBasketRef.innerHTML = '';
+
+    for (let b = 0; b < shoppingBasket.length; b++) {
+        let dishBasketName = shoppingBasket[b].name;
+        let dishBasketPrice = shoppingBasket[b].price;
+        let dishBasketAmount = shoppingBasket[b].amount;
+
+        shoppingBasketRef.innerHTML += getBasketTemplate(dishBasketName, dishBasketPrice, dishBasketAmount);
+    }
+}
+
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,4 +1,4 @@
-function getDishesTemplate(NAME, INFO, PRICE) {
+function getDishesTemplate(NAME, INFO, PRICE, index) {
     return `
         <div class="dish-card">
             <div class="dish-card-left">
@@ -6,7 +6,20 @@ function getDishesTemplate(NAME, INFO, PRICE) {
                 ${INFO}
                 <span class="price-tag">${PRICE.toFixed(2).toString().replace(".", ",")} €</span>
             </div class="dish-card-right">
-                <img class="add-img" src="./assets/icons/add.png" alt="">  
+                <img onclick="addToShoppingBasket(${index})" class="add-img" src="./assets/icons/add.png" alt="">  
+        </div>
+        `;
+}
+
+function getBasketTemplate(dishBasketName, dishBasketPrice, dishBasketAmount) {
+    return `
+        <div>${dishBasketName}</div>
+        <div class="dish-basket-info">    
+            <div> - </div>
+            <div>${dishBasketAmount}x</div>
+            <div> + </div>
+            <div>${dishBasketPrice.toFixed(2).toString().replace('.', ',')} €</div>
+            <div> x </div>
         </div>
         `;
 }
